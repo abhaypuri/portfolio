3f5bdde2813c0806e57dbc9519aebe60a13ec340
Rahul Syal <syal2512@gmail.com>
2019-07-22 01:55:29 +0530

    Updates

---
+++ precache-manifest.492f085cfbbc004d3bfb0cbfa90dc1a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe29f86538492ec2a1f7564369ce053",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c90f8acb5190d4661c6c",
+    "url": "/portfolio/static/css/main.513f5184.chunk.css"
+  },
+  {
+    "revision": "37a4c17d7b94a4171ff4",
+    "url": "/portfolio/static/js/2.68f13cf6.chunk.js"
+  },
+  {
+    "revision": "c90f8acb5190d4661c6c",
+    "url": "/portfolio/static/js/main.793ea281.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2ba6373c2e2e2503c7b7546b58aab179",
+    "url": "/portfolio/static/media/CV_AbhayPuri_Intern.2ba6373c.pdf"
+  },
+  {
+    "revision": "d1390aa8625fb7f5e86e585cabd086a9",
+    "url": "/portfolio/static/media/me.d1390aa8.jpg"
+  }
+]);
